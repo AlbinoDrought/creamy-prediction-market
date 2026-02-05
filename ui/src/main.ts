ad96e813f806ce9b660b7229e95de6d8ca4a1985
Sean <sean@albinodrought.com>
2026-02-04 23:40:38 -0800

    feat: advertise own source

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -5,6 +5,12 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+console.log(`
+# AlbinoDrought/creamy-prediction-market
+Repo: https://github.com/AlbinoDrought/creamy-prediction-market
+Source: ${window.location.origin}/source.tar.gz
+`);
+
 const app = createApp(App)
 
 app.use(createPinia())
