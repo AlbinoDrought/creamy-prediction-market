fc091794216832f8b95f31637d4ecf50585f18fc
Sean <sean@albinodrought.com>
2026-02-05 00:14:24 -0800

    feat: toast on win

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,10 +1,33 @@
 <script setup lang="ts">
-import { onMounted, watch } from 'vue'
+import { onMounted, watch, ref, computed } from 'vue'
 import { useAuthStore } from '@/stores/auth'
+import { useBetsStore } from '@/stores/bets'
 import { useSSE } from '@/composables/useSSE'
+import Toast from '@/components/Toast.vue'
 
 const authStore = useAuthStore()
+const betsStore = useBetsStore()
 const { connect } = useSSE()
+
+const showWinToast = ref(false)
+const winMessage = ref('')
+
+const totalWinnings = computed(() => {
+  return betsStore.newlyWonBets.reduce((sum, bet) => sum + (bet.won_amount ?? 0), 0)
+})
+
+watch(() => betsStore.newlyWonBets, (newWins) => {
+  if (newWins.length > 0) {
+    const totalWon = newWins.reduce((sum, bet) => sum + (bet.won_amount ?? 0), 0)
+    winMessage.value = `You won ${totalWon} tokens!`
+    showWinToast.value = true
+  }
+}, { deep: true })
+
+function closeWinToast() {
+  showWinToast.value = false
+  betsStore.clearNewlyWonBets()
+}
 
 onMounted(async () => {
   // Try to restore session on app load
@@ -21,4 +44,11 @@ watch(() => authStore.token, () => {
 
 <template>
   <RouterView />
+  <Toast
+    v-if="showWinToast"
+    :message="winMessage"
+    type="success"
+    :duration="5000"
+    @close="closeWinToast"
+  />
 </template>
